16d63746e987a1ebb8c9c11d0dea4d1208ebd6b1
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-15 15:45:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,8 @@
-name = "Ben"
-language = "Ruby"
+
 
 def introduction_with_language (name, language)
+  name = "Ben"
+  language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
